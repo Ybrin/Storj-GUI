7daf24da27e8851c47b508d530b95e2f17d2e734
Koray Koska <koray@koska.at>
2017-09-14 02:43:51 +0200

    Initial Commit

---
+++ Storj/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Storj
+//
+//  Created by Koray Koska on 14/09/2017.
+//  Copyright © 2017 Koray Koska. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Storj/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Storj
+//
+//  Created by Koray Koska on 14/09/2017.
+//  Copyright © 2017 Koray Koska. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
